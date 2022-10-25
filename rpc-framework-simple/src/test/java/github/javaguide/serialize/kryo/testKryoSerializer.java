914c7cc646804aec6072d4d9cb4bdc3b278ba4fd
zhp <zhp1223912573@qq.com>
2022-10-25 15:00:15 +0800

    compress-kryo-testwithExtensionLoader

--- rpc-framework-simple/src/test/java/github/javaguide/serialize/kryo/testKryoSerializer.java
+++ rpc-framework-simple/src/test/java/github/javaguide/serialize/kryo/testKryoSerializer.java
@@ -1,5 +1,7 @@
 package github.javaguide.serialize.kryo;
 
+import github.javaguide.extension.ExtensionLoader;
+import github.javaguide.serialize.Serializer;
 import org.checkerframework.checker.units.qual.K;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +20,17 @@ public class testKryoSerializer {
         assert s1.getName().equals(deserialize.getName());
         assert s1.getGender().equals(deserialize.getGender());
 
+    }
+
+    @Test
+    public void testKryoWithExtensionsLoader(){
+
+        ExtensionLoader<Serializer> extensionLoader = ExtensionLoader.getExtensionLoader(Serializer.class);
+        Serializer kryoSerializer = extensionLoader.getExtension("kryo");
+        student s1 = new student("zhp","nan");
+        byte[] serialize = kryoSerializer.serialize(s1);
+        student deserialize = kryoSerializer.deserialize(serialize, student.class);
+        assert s1.getName().equals(deserialize.getName());
+        assert s1.getGender().equals(deserialize.getGender());
     }
 }
